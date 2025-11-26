111506b18e17cd23b0e361128ecd3de0d092a63d
buleezene <buleezene@gmail.com>
2025-11-26 07:16:41 +0100

    Add files via upload

---
+++ csontvázak/pythoncsontvazak/pythoncsontváz.py
@@ -0,0 +1,24 @@
+"""
+Készítette: Bőhm Balázs
+Osztály: 2025-2026 I/1/E
+Dátum:
+"""
+
+print(__doc__)
+
+#OSZTÁLY
+#class :
+#    def __init__(self,sor):
+#        reszek=sor.split(":")
+#        self. 
+#        self.
+
+
+
+#IMPORTolvasás
+#with open("fájlnév.txt", "r", encoding="utf-8") as file:
+
+#IMPORTírás
+#file=open("fájlnév.txt", "w", encoding="utf-8")
+#file.close()
+
